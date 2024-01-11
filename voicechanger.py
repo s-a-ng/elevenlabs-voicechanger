df2400c24f49a51a94fed9d69131fc2adc06a63b
sang . gnas <61368081+s-a-ng@users.noreply.github.com>
2024-01-10 19:51:16 -0800

    major overhaul
    made recording process audio in async to speed up the delay

--- voicechanger.py
+++ voicechanger.py
@@ -1,18 +1,37 @@
-from threading import Thread
 from pygame import mixer 
+from pydub import AudioSegment
 from termcolor import colored
-import speech_recognition as sr
-import requests, json
-import io
-import os
-import time
-import re 
-os.system("cls")
+from threading import Thread
+
+import io, os, re
+import time, wave, json
+
+import pyaudio
+
+import requests
+
+import webrtcvad, requests
+
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+clear()
 time.sleep(1)
 
 voices_map = {
 
 }
+
+def threaded_function(func):
+    def wrapper(*args, **kwargs):
+        t = Thread(target=func, args=args, kwargs=kwargs)
+        t.daemon = True
+        t.start()
+        return t
+    return wrapper
+
+
 
 def remove_emojis(text):
     emoji_pattern = re.compile("["
@@ -98,33 +117,181 @@ while True:
         print(colored("Invalid voice", "red"))
 
 
-os.system("cls")
-
+clear()
 mixer.init() 
 mixer.quit() 
 mixer.init(devicename='CABLE Input (VB-Audio Virtual Cable)') 
 
-def record_audio():
-    recognizer = sr.Recognizer()
-    with sr.Microphone() as source:
-        recognizer.adjust_for_ambient_noise(source)
-        print(colored("Listening", "green"))
-        while True: 
-            audio = recognizer.listen(source)
-            #a = time.time()
-            try:
-                transcription = recognizer.recognize_google(audio) # detects voice activity better. if it errors it could not pick up any speech
-                #print("You said", transcription)
-                #print(time.time()-a)
-                return io.BytesIO(audio.get_wav_data())
-            except sr.UnknownValueError:
-                continue
+RATE = 16000
+CHANNELS = 1
+CHUNK = 480
+RECORD_SECONDS = 3
 
-def play_audio(audio_file):
+
+def convert_wav_buffer_to_flac(wav_buffer):
+    sound = AudioSegment.from_wav(wav_buffer)
+
+    sound = sound.set_sample_width(2)
+
+    flac_buffer = io.BytesIO()
+    sound.export(flac_buffer, format="flac")
+    raw_flac_bytes = flac_buffer.getvalue()
+
+    return raw_flac_bytes
+
+voice_chunks = {}
+chunk_index = 0
+
+class AudioChunk:
+    def __init__(self, wav_buffer):
+        global chunk_index
+        self.wav_buffer = wav_buffer
+        self.flac_data = convert_wav_buffer_to_flac(wav_buffer)
+
+        self.eleven_labs_data = None
+
+        self.elevenlabs_processed_flag = False
+        self.transcription_processed_flag = False
+
+        self.transcription = ""
+        self.index = chunk_index
+        voice_chunks[chunk_index] = self
+        chunk_index += 1 
+    
+    def remove_chunk(self):
+        #print("Removing chunk", self.index)
+        del voice_chunks[self.index]
+
+    @threaded_function
+    def transcribe(self): # scuffed but works better than webrtcvad by itself 
+        url = f"http://www.google.com/speech-api/v2/recognize?client=chromium&lang=en-US&key=AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw"
+        headers = {"Content-Type": "audio/x-flac; rate=" + str(RATE)}
+        response = requests.post(url, data=self.flac_data, headers=headers)
+
+        try: 
+            response_json = json.loads(response.text.split('\n', 1)[1])
+            transcript = response_json["result"][0]["alternative"][0]["transcript"]
+            self.transcription_processed_flag = True
+            self.transcription = transcript
+
+            self.apply_speech_to_speech_async()
+
+            #print("sucessfully transcribed speech, calling sts")
+        except Exception as e:
+            self.remove_chunk()
+    
+    @threaded_function
+    def apply_speech_to_speech_async(self):
+        try: 
+            url = f"https://api.elevenlabs.io/v1/speech-to-speech/{voice_id}/stream"
+            headers = {
+                "Authorization" : Bearer
+            }
+            data = {
+                "model_id": "eleven_english_sts_v2",
+                "voice_settings" : json.dumps({
+                    "stability": 0.4,
+                    "similarity_boost": 0.3
+                }),
+            }
+
+            self.wav_buffer.seek(0) 
+            files = {'audio': ('audio.wav', self.wav_buffer, 'audio/wav')}
+
+            response = requests.post(url, headers=headers, data=data, files=files)
+
+            content = response.content
+            if len(content) <= 200: 
+                try:
+                    load = json.loads(content)
+                    details = load["detail"]
+                    error_type = details["status"]
+                    message = details["message"]
+
+                    print(f'''
+    {colored("ElevenLabs returned an error", "light_red")}:
+        {colored(f'- Error type: "{error_type}"', "red")}
+        {colored(f'- More details: "{message}"', "red")}
+
+    {colored(f"There is a good chance this error is because your Bearer token expired.{chr(10)}Please try restarting the program", "red")}
+                    ''')
+                    self.remove_chunk()
+                    return 
+                except Exception:
+                    self.remove_chunk()
+                    return 
+
+            self.eleven_labs_data = io.BytesIO(content) 
+            self.elevenlabs_processed_flag = True 
+            #print("applied sts on", self.index)
+        except Exception:
+            self.remove_chunk()
+            return 
+
+
+
+vad = webrtcvad.Vad() 
+vad.set_mode(3) # most aggressive (1-3)
+
+audio = pyaudio.PyAudio()
+stream = audio.open(format=pyaudio.paInt16,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
+
+
+
+def record_audio():
+    while True:
+        frames = []
+        num_silent_chunks = 0
+        noise_chunks = 0
+
+        one_second = RATE // CHUNK
+        frame_cap = one_second * RECORD_SECONDS
+        
+        while True:
+            data = stream.read(CHUNK)
+            is_speech_chunk = vad.is_speech(data, RATE)
+
+            if is_speech_chunk:
+                num_silent_chunks = 0
+                noise_chunks += 1 
+            else:
+                num_silent_chunks += 1
+
+            frames.append(data)
+            if len(frames) >= frame_cap and num_silent_chunks >= one_second:
+                silence_percent = noise_chunks / frame_cap * 100
+
+                print(colored(f"{silence_percent}% of chunk detected as speech", "blue"))
+
+                if silence_percent < 20:
+                    print("mostly silent")
+                    break
+
+                print("threshold reached")
+                wav_buffer = io.BytesIO()
+                with wave.open(wav_buffer, 'wb') as wf:
+                    wf.setnchannels(CHANNELS)
+                    wf.setsampwidth(audio.get_sample_size(pyaudio.paInt16))
+                    wf.setframerate(RATE)
+                    wf.writeframes(b''.join(frames))
+
+                wav_buffer.seek(0) 
+
+                chunk = AudioChunk(wav_buffer)
+                chunk.transcribe()
+
+                break
+
+def play_audio(chunk):
+    print(colored("Playing audio chunk", "green"))
     file_path = "temp_audio.mp3"
 
     with open(file_path, 'wb') as f:
-        f.write(audio_file.getvalue())
+        f.write(chunk.eleven_labs_data.getvalue())
 
 
     mixer.music.load(file_path)
@@ -137,108 +304,42 @@ def play_audio(audio_file):
     mixer.music.stop()
     mixer.music.unload()
     os.remove(file_path)
+    chunk.remove_chunk()
 
-def transform_speech_endpoint(audio_file):
-    try: 
-        url = f"https://api.elevenlabs.io/v1/speech-to-speech/{voice_id}/stream"
-        headers = {
-            "Authorization" : Bearer
-        }
-        data = {
-            "model_id": "eleven_english_sts_v2",
-            "voice_settings" : json.dumps({
-                "stability": 0.4,
-                "similarity_boost": 0.3
-            }),
-        }
+def play_audio_chunks(): # Race condition galore
+    while True:
+        if not voice_chunks:  
+            continue
 
-        audio_file.seek(0) 
-        files = {'audio': ('audio.wav', audio_file, 'audio/wav')}
+        keys = list(voice_chunks.keys())
 
-        response = requests.post(url, headers=headers, data=data, files=files)
+        if len(keys) == 0:
+            continue
+        
+        min_key = min(keys)
+        chunk = voice_chunks.get(min_key)
 
-        content = response.content
-        if len(content) <= 200: 
-            try:
-                load = json.loads(content)
-                details = load["detail"]
-                error_type = details["status"]
-                message = details["message"]
+        if not chunk:
+            continue
 
-                print(f'''
-{colored("ElevenLabs returned an error", "light_red")}:
-    {colored(f'- Error type: "{error_type}"', "red")}
-    {colored(f'- More details: "{message}"', "red")}
+        try:
+            if chunk.transcription_processed_flag and chunk.elevenlabs_processed_flag:
+                break
+        except AttributeError: 
+            continue
 
-{colored(f"There is a good chance this error is because your Bearer token expired.{chr(10)}Please try restarting the program", "red")}
-                ''')
-
-                return None
-            except Exception:
-                return None
-
-        return io.BytesIO(content)  
-    except Exception as e:
-        return None
-
-def threaded_function(func):
-    def wrapper(*args, **kwargs):
-        t = Thread(target=func, args=args, kwargs=kwargs)
-        t.daemon = True
-        t.start()
-        return t
-    return wrapper
-
-chunk_index = 0
-audio_queue = {}
-voicechanger_active = True
-
-@threaded_function
-def transform_speech(chunk):
-    audio_data = chunk["audio_blob"]
-    new_data = transform_speech_endpoint(audio_data)  
-    chunk["processed_flag"] = True
-    if not new_data:
-        chunk["failed"] = True
-        return 
-    chunk["audio_blob"] = new_data
-
-@threaded_function
-def record():
-    global chunk_index, audio_queue, voicechanger_active
-    
-    while voicechanger_active:
-        audio = record_audio()
-        if audio: 
-            audio_queue[chunk_index] = {
-                "audio_blob" : audio, 
-                "failed" : False, 
-                "processed_flag" : False
-            }
-            transform_speech(audio_queue[chunk_index])
-            chunk_index += 1
+    play_audio(chunk) 
 
 def main():
     try:
-        global audio_queue, voicechanger_active
         while True:
-            if not audio_queue:  
-                continue
-            first_index = min(audio_queue.keys())
-            data = audio_queue.pop(first_index)                
-            while not data["processed_flag"]:
-                continue
-                
-            if data["failed"]:
-                print(colored("Failed to convert this chunk. There may be something wrong with your ElevenLabs account.", "red"))
-                continue
-            print(colored("Playing audio chunk", "green"))
-            play_audio(data["audio_blob"]) 
-
+            play_audio_chunks()
     except KeyboardInterrupt:
         print("Stopping")
-        voicechanger_active = False
+        stream.stop_stream()
+        stream.close()
+        audio.terminate()
         exit()
 
-record()
+record_audio()
 main()
